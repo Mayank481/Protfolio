b9a8d872e1aa2f600789aa16c5299122715d547e
Mayank481 <jaggi.mayank19@gmail.com>
2024-08-12 17:28:31 +0530

    add full

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,7 +1,72 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Contact.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export const Contact = () => {
+  const [inputValue, setInputValue] = useState({
+    FirstName: "",
+    LastName: "",
+    Email: "",
+    PhoneNumber: "",
+    Message: "",
+  });
+
+  const getValue = (e) => {
+    const { name, value } = e.target;
+    setInputValue(() => {
+      return {
+        ...inputValue,
+        [name]: value,
+      };
+    });
+  };
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <div className="container mb-3 contact">
+        <h2 className="text-center">Contact Us</h2>
+        <div className="container mt-2">
+          <Form className="row mt-2">
+            <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control type="text" onChange={getValue} name="FirstName" />
+            </Form.Group>
+            <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control type="text" onChange={getValue} name="LastName" />
+            </Form.Group>
+            <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" onChange={getValue} name="Email" />
+            </Form.Group>
+            <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+              <Form.Label>Phone Number</Form.Label>
+              <Form.Control
+                type="text"
+                onChange={getValue}
+                name="PhoneNumber"
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Message</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={4}
+                onChange={getValue}
+                name="Message"
+              />
+            </Form.Group>
+            <div className="d-flex justify-content-center">
+              <Button variant="primary" className="col-lg-6" type="submit">
+                Submit
+              </Button>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
+};
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
 
 export const Footer = () => {
+  const year = new Date().getUTCFullYear();
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="container footer_container d-flex justify-content-around flex-wrap">
+        <div className="first mt-5">
+          <h4>Mayank Jaggi</h4>
+          <p>© {year} Mayank Jaggi All right reserved</p>
+          <p className="d-flex">
+            <i className="fa-brands fa-instagram"></i>
+            <i className="fa-brands fa-facebook mx-3"></i>
+            <i className="fa-brands fa-github"></i>
+          </p>
+        </div>
+        <div className="second mt-5">
+          <h4>Get in Touch</h4>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.</p>
+          <p>jaggi.mayank19@gmail.com</p>
+          <p>+91-8400547870</p>
+        </div>
+        <div className="third mt-5">
+          <p>About</p>
+          <p>Resume</p>
+        </div>
+      </div>
+    </div>
+  );
+};
